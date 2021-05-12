41a3d2ece1578d4b619050f1b0e15c14a613b657
Ludwik Papaj <ludwikpapaj@gmail.com>
2021-05-12 08:34:57 +0200

    3 div instead of 1, graphics

--- django_horoscope/Horoscope/pages/views.py
+++ django_horoscope/Horoscope/pages/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from divinations.models import Divination  #i might do it in wrong file /  also can be  a name issue 
 from zodiacs.models import Zodiac
 from .forms import GiveYearForm
+#from datetime import datetime, timedelta, date
 from datetime import datetime
+from datetime import timedelta
 
 def home_view(request, *args, **kwargs):
     all_zodiacs = Zodiac.objects.all()
@@ -14,21 +16,37 @@ def home_view(request, *args, **kwargs):
     
 def zodiac_page_view(request, id_, *args, **kwargs):
     now = datetime.now()
-    day_date = now.strftime("%d %b %Y") 
-    day_ = int(now.strftime("%d"))
-    month_ = int(now.strftime("%m"))
+    tomorrow = now + timedelta(days=1)
+    after_tomorrow = now + timedelta(days=2)   
+     
+    day_today = int(now.strftime("%d"))
+    month_today = int(now.strftime("%m"))
+    day_tomorrow = int(tomorrow.strftime("%d"))
+    month_tomorrow = int(tomorrow.strftime("%m"))
+    day_after_tomorrow = int(after_tomorrow.strftime("%d"))
+    month_after_tomorrow = int(after_tomorrow.strftime("%m"))    
 
-    obj_dev = devination_for_a(day_, month_, id_)
+    obj_dev1 = devination_for_a(day_today, month_today, id_)
+    obj_dev2 = devination_for_a(day_tomorrow, month_tomorrow, id_)
+    obj_dev3 = devination_for_a(day_after_tomorrow, month_after_tomorrow, id_)
+    
+    day1 = now.strftime("%d %b %Y") 
+    day2 = tomorrow.strftime("%d %b %Y")
+    day3 =  after_tomorrow.strftime("%d %b %Y") 
     obj_zod = Zodiac.objects.get(id=id_) 
     my_context = {
-        'description': obj_dev.description,
-        'day_date': day_date,
+        'description1': obj_dev1.description,
+        'description2': obj_dev2.description,
+        'description3': obj_dev3.description,
+        'day_today': day1,
+        'day_tomorrow': day2,
+        'day_after_tomorrow': day3,
         'zodiac_sign': obj_zod,   
     }
     return (render(request, "zodiac_page.html", my_context))
 
 # it is not a view but for clarity this funtion is below the zodiac_page_view
-def devination_for_a(day, month, zodiac_id):
+def devination_for_a(day, month, zodiac_id, ):
     all_divination = Divination.objects.all()
     base_number = day*month*zodiac_id
     wynik = base_number%len(all_divination) 
