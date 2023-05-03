6d7a407c76229ff75b224bb22b6aa7ac0b9db00e
Ludwik <ludwikpapaj@gmail.com>
2023-05-03 23:31:28 +0200

    settings update

--- django_horoscope/Horoscope/Horoscope/settings.py
+++ django_horoscope/Horoscope/Horoscope/settings.py
@@ -133,3 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 #MEDIA_URL = '/media/'
 
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+#  TEST to debug recent 3 WARNINGS
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
